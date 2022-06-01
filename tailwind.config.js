0251a700234e1395e6aae847443f2da41d1f30ea
joeephwild <josephomotade0@gmail.com>
2022-06-01 09:52:11 -0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,55 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      backgroundImage: theme => ({
+        "header-img": "url('/header.jpg')",
+        "cover1-img": "url('/cover-1.jpg')",
+        "cover2-img": "url('/cover-2.jpg')",
+        "cover3-img": "url('/cover-3.jpg')",
+        "cover4-img": "url('/cover-4.jpg')",
+        "livesport-img": "url('/live-sports.jpg')",
+        "livesport-small": "url('/live-sports-small.jpg')",
+        "network-logo": "url('/network-logo-bg.png')"
+      }),
+      screens: {
+        "3xl": "2000px",
+      },
+      colors: {
+        "hulu-green": "#00ed82",
+        "live-section": "#151516"
+      },
+      width: {
+        "300": "300px",
+        "250": "250px",
+        "270": "270px",
+        "200": "200px"
+      },
+      height: {
+        "180": "180px",
+        "500": "500px",
+        "530": "530px",
+        "800": "800px"
+      },
+      inset: {
+        "160": "160px",
+        "100": "100px"
+      },
+      maxWidth: {
+        "550": "550px",
+        "1100": "1100px"
+      }
+    },
+    gradientColorStops: theme => ({
+      ...theme("colors"),
+      cstart: "#0f495c",
+      cvia: "#0d3640",
+      cend: "#08141f"
+    })
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: [require("tailwind-scrollbar-hide")],
+};
